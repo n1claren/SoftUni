6236031dcd22a2e8e07cfb5ded6b4bbb438f0a01
Nikolay Ivanov <nikolay1892@abv.bg>
2020-12-23 01:35:08 +0200

    basic syntax summary, more exercises

---
+++ 1.Programming-Fundamentals-with-C#/03.Basic-Syntax-Conditional-Statements-And-Loops-MoreExercise/03.Gaming-Store/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace _03.Gaming_Store
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double currentBalance = double.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+
+            double totalSpent = 0.0;
+
+            while (input != "Game Time")
+            {
+                double price = 0.0;
+                bool gameFound = true;
+
+                switch (input)
+                {
+                    case "OutFall 4":
+                        price = 39.99;
+                        break;
+
+                    case "CS: OG":
+                        price = 15.99;
+                        break;
+
+                    case "Zplinter Zell":
+                        price = 19.99;
+                        break;
+
+                    case "Honored 2":
+                        price = 59.99;
+                        break;
+
+                    case "RoverWatch":
+                        price = 29.99;
+                        break;
+
+                    case "RoverWatch Origins Edition":
+                        price = 39.99;
+                        break;
+
+                    default:
+                        Console.WriteLine("Not Found");
+                        gameFound = false;
+                        break;
+                }
+
+                if (gameFound)
+                {
+                    if (currentBalance >= price)
+                    {
+                        currentBalance -= price;
+                        totalSpent += price;
+
+                        Console.WriteLine($"Bought {input}");
+
+                        if (currentBalance == 0)
+                        {
+                            Console.WriteLine("Out of money!");
+                            input = "Game Time";
+                            continue;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Too Expensive");
+                        input = Console.ReadLine();
+                        continue;
+                    }
+                }
+                else
+                {
+                    input = Console.ReadLine();
+                    continue;
+                }
+                
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Total spent: ${totalSpent:F2}. Remaining: ${currentBalance:f2}");
+        }
+    }
+}
---
+++ 1.Programming-Fundamentals-with-C#/03.Basic-Syntax-Conditional-Statements-And-Loops-MoreExercise/05.Messages/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+
+namespace _05.Messages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                int digit = int.Parse(input[0].ToString());
+
+                switch (digit)
+                {
+                    case 0:
+                        Console.Write(" ");
+                        break;
+                    case 2:
+                        if (input.Length == 1)
+                        {
+                            Console.Write("a");
+                        }
+                        else if (input.Length == 2)
+                        {
+                            Console.Write("b");
+                        }
+                        else if (input.Length == 3)
+                        {
+                            Console.Write("c");
+                        }
+                        break;
+                    case 3:
+                        if (input.Length == 1)
+                        {
+                            Console.Write("d");
+                        }
+                        else if (input.Length == 2)
+                        {
+                            Console.Write("e");
+                        }
+                        else if (input.Length == 3)
+                        {
+                            Console.Write("f");
+                        }
+                        break;
+                    case 4:
+                        if (input.Length == 1)
+                        {
+                            Console.Write("g");
+                        }
+                        else if (input.Length == 2)
+                        {
+                            Console.Write("h");
+                        }
+                        else if (input.Length == 3)
+                        {
+                            Console.Write("i");
+                        }
+                        break;
+                    case 5:
+                        if (input.Length == 1)
+                        {
+                            Console.Write("j");
+                        }
+                        else if (input.Length == 2)
+                        {
+                            Console.Write("k");
+                        }
+                        else if (input.Length == 3)
+                        {
+                            Console.Write("l");
+                        }
+                        break;
+                    case 6:
+                        if (input.Length == 1)
+                        {
+                            Console.Write("m");
+                        }
+                        else if (input.Length == 2)
+                        {
+                            Console.Write("n");
+                        }
+                        else if (input.Length == 3)
+                        {
+                            Console.Write("o");
+                        }
+                        break;
+                    case 7:
+                        if (input.Length == 1)
+                        {
+                            Console.Write("p");
+                        }
+                        else if (input.Length == 2)
+                        {
+                            Console.Write("q");
+                        }
+                        else if (input.Length == 3)
+                        {
+                            Console.Write("r");
+                        }
+                        else if (input.Length == 4)
+                        {
+                            Console.Write("s");
+                        }
+                        break;
+                    case 8:
+                        if (input.Length == 1)
+                        {
+                            Console.Write("t");
+                        }
+                        else if (input.Length == 2)
+                        {
+                            Console.Write("u");
+                        }
+                        else if (input.Length == 3)
+                        {
+                            Console.Write("v");
+                        }
+                        break;
+                    case 9:
+                        if (input.Length == 1)
+                        {
+                            Console.Write("w");
+                        }
+                        else if (input.Length == 2)
+                        {
+                            Console.Write("x");
+                        }
+                        else if (input.Length == 3)
+                        {
+                            Console.Write("y");
+                        }
+                        else if (input.Length == 4)
+                        {
+                            Console.Write("z");
+                        }
+                        break;
+                }
+
+            }
+        }
+    }
+}
