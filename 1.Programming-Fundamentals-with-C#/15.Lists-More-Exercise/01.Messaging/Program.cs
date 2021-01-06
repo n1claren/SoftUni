1a78e8f1fa98b5356ce46db196a8eda0841078c1
Nikolay Ivanov <nikolay1892@abv.bg>
2021-01-06 22:07:01 +0200

    Lists more exercise

---
+++ 1.Programming-Fundamentals-with-C#/15.Lists-More-Exercise/01.Messaging/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Messaging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string text = Console.ReadLine();
+
+            string result = string.Empty;
+
+            List<int> singles = new List<int>();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                string tempString = numbers[i].ToString();
+
+                int tempNumber = 0;
+
+                for (int z = 0; z < tempString.Length; z++)
+                {
+                    tempNumber += int.Parse(tempString[z].ToString());
+                }
+
+                singles.Add(tempNumber);
+            }
+
+            for (int i = 0; i < singles.Count; i++)
+            {
+                int index = singles[i];
+
+                while (index > text.Length)
+                {
+                    index -= text.Length;
+                }
+
+                result += text[index].ToString();
+
+                text = text.Remove(index, 1);
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ 1.Programming-Fundamentals-with-C#/15.Lists-More-Exercise/02.Car-Race/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.Car_Race
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> race = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            double leftRacerTime = 0.0;
+            double rightRacerTime = 0.0;
+
+            for (int i = 0; i < race.Count / 2; i++)
+            {
+                leftRacerTime += race[i];
+
+                if (race[i] == 0)
+                {
+                    leftRacerTime *= 0.8;
+                }
+
+            }
+
+            for (int i = race.Count - 1; i > race.Count / 2; i--)
+            {
+                rightRacerTime += race[i];
+
+                if (race[i] == 0)
+                {
+                    rightRacerTime *= 0.8;
+                }
+
+            }
+
+            if (leftRacerTime < rightRacerTime)
+            {
+                Console.WriteLine($"The winner is left with total time: {leftRacerTime}");
+            }
+            else
+            {
+                Console.WriteLine($"The winner is right with total time: {rightRacerTime}");
+            }
+        }
+    }
+}
