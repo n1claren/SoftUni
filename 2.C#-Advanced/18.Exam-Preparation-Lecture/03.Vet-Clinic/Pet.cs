c2b8f75316b3e4587ba12acc332a1bf4f17e4003
Nikolay Ivanov <nikolay1892@abv.bg>
2021-02-18 18:41:43 +0200

    exam prep

---
+++ 2.C#-Advanced/18.Exam-Preparation-Lecture/03.Vet-Clinic/Pet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VetClinic
+{
+    public class Pet
+    {
+        public Pet(string name, int age, string owner)
+        {
+            this.Age = age;
+            this.Name = name;
+            this.Owner = owner;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Owner { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name} Age: {Age} Owner: {Owner}";
+        }
+    }
+}
