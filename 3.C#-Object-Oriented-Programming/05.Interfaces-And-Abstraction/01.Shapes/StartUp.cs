0ac2a68be2c5dd1fa5fd2a5a83be8e98590051b9
Nikolay Ivanov <nikolay1892@abv.bg>
2021-03-05 20:29:00 +0200

    interfaces and abstraction

---
+++ 3.C#-Object-Oriented-Programming/05.Interfaces-And-Abstraction/01.Shapes/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            IDrawable circle = new Circle(6);
+
+            List<IDrawable> drawableObjects = new List<IDrawable>();
+
+            drawableObjects.Add(new Rectangle(7, 2));
+
+            drawableObjects.Add(new Circle(3));
+
+            drawableObjects.Add(new Rectangle(9, 4));
+
+            drawableObjects.Add(new Circle(6));
+
+            drawableObjects.Add(new Rectangle(2, 7));
+
+            drawableObjects.Add(new Circle(9));
+
+            foreach (var item in drawableObjects)
+            {
+                item.Draw();
+            }
+        }
+    }
+}
