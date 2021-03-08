26b2bdb19f82098771e5f233e4eda7ce5fac89a0
Nikolay Ivanov <nikolay1892@abv.bg>
2021-03-08 22:48:10 +0200

    polymorphism lecture

---
+++ 3.C#-Object-Oriented-Programming/07.Polymorphism/03.Shapes/Circle.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        public override string Draw()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            double rIn = radius - 0.4;
+            double rOut = radius + 0.4;
+
+            for (double y = radius; y >= -radius; --y)
+            {
+                for (double x = -radius; x < rOut; x += 0.5)
+                {
+                    double value = x * x + y * y;
+                    if (value >= rIn * rIn && value <= rOut * rOut)
+                        sb.Append("*");
+                    else
+                        sb.Append(" ");
+                }
+
+                sb.AppendLine();
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ 3.C#-Object-Oriented-Programming/07.Polymorphism/03.Shapes/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(8);
+            Rectangle rectangle = new Rectangle(12, 8);
+
+            Console.WriteLine(circle.CalculateArea());
+            Console.WriteLine(circle.CalculatePerimeter());
+            Console.WriteLine(circle.Draw());
+
+            Console.WriteLine(rectangle.CalculateArea());
+            Console.WriteLine(rectangle.CalculatePerimeter());
+            Console.WriteLine(rectangle.Draw());
+        }
+    }
+}
