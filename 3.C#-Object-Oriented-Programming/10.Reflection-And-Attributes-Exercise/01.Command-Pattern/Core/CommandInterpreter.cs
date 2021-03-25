b370c93f6945356b95cba3ebf04fd93dd589cc10
Nikolay Ivanov <nikolay1892@abv.bg>
2021-03-25 17:41:29 +0200

    reflection and attributes exercise

---
+++ 3.C#-Object-Oriented-Programming/10.Reflection-And-Attributes-Exercise/01.Command-Pattern/Core/CommandInterpreter.cs
@@ -0,0 +1,43 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace CommandPattern.Core
+{
+    public class CommandInterpreter : ICommandInterpreter
+    {
+        private const string COMMAND_POSTFIX = "Command";
+
+        public CommandInterpreter()
+        {
+
+        }
+
+        public string Read(string args)
+        {
+            string[] commandInput = args.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string commandName = commandInput[0] + COMMAND_POSTFIX;
+
+            string[] commandArgs = commandInput.Skip(1).ToArray();
+
+            Assembly assembly = Assembly.GetCallingAssembly();
+
+            Type commandType = assembly
+                .GetTypes()
+                .FirstOrDefault(t => t.Name.ToLower() == commandName.ToLower());
+
+            if (commandType == null)
+            {
+                throw new ArgumentException("Invalid command type!");
+            }
+
+            ICommand commandInstance = (ICommand)Activator.CreateInstance(commandType);
+
+            string result = commandInstance.Execute(commandArgs);
+
+            return result;
+        }
+    }
+}
---
+++ 3.C#-Object-Oriented-Programming/10.Reflection-And-Attributes-Exercise/01.Command-Pattern/Core/Engine.cs
@@ -0,0 +1,34 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+
+namespace CommandPattern.Core
+{
+    public class Engine : IEngine
+    {
+        private readonly ICommandInterpreter commandInterpreter;
+
+        public Engine(ICommandInterpreter CI)
+        {
+            commandInterpreter = CI;
+        }
+
+        public void Run()
+        {
+            while (true)
+            {
+                string args = Console.ReadLine();
+
+                try
+                {
+                    string result = commandInterpreter.Read(args);
+
+                    Console.WriteLine(result);
+                }
+                catch (ArgumentException ae)
+                {
+                    Console.WriteLine(ae.Message);
+                }
+            }
+        }
+    }
+}
