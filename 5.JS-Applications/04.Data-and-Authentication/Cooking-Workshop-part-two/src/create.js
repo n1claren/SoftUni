318211742f20ed9cf57ce3f25c93387933a6be24
Nikolay Ivanov <nikolay1892@abv.bg>
2021-07-17 19:54:43 +0300

    cooking workshop part 2

---
+++ 5.JS-Applications/04.Data-and-Authentication/Cooking-Workshop-part-two/src/create.js
@@ -0,0 +1,39 @@
+document.querySelector('form').addEventListener('submit', onCreateSubmit);
+
+async function onCreateSubmit(ev) {
+    ev.preventDefault();
+
+    const formData = new FormData(ev.target);
+
+    const name = formData.get('name');
+    const image = formData.get('img');
+
+    const ingredients = formData
+        .get('ingredients')
+        .split('\n')
+        .map(l => l.trim())
+        .filter(l => l != '');
+
+    const steps = formData
+        .get('steps')
+        .split('\n')
+        .map(l => l.trim())
+        .filter(l => l != '');
+
+    const token = sessionStorage.getItem('userToken');
+
+    const response = await fetch('http://localhost:3030/data/recipes', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json',
+                   'X-Authorization': token
+        },
+        body: JSON.stringify({ name, image, ingredients, steps })
+    })
+
+    if (response.ok != true) {
+        const error = await response.json();
+        return alert(error.message);
+    }
+
+    window.location.pathname = 'index.html';
+}
