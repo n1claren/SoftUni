47b340495979a8de836488b7270df74672facf54
Nikolay Ivanov <nikolay1892@abv.bg>
2021-11-12 21:21:07 +0200

    entity relations student problem

---
+++ 7.Entity-Framework-Core/02.Entity-Relations/Entity-Relations-Students-System/P01_StudentSystem.Data/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace P01_StudentSystem.Data
+{
+    public class Configuration
+    {
+        public const string CONNECTION_STRING =
+            @"Server=DESKTOP-UHNU3OE\SQLEXPRESS;Database=Students_System;Integrated Security=True;";
+    }
+}
---
+++ 7.Entity-Framework-Core/02.Entity-Relations/Entity-Relations-Students-System/P01_StudentSystem/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using P01_StudentSystem.Data;
+
+namespace P01_StudentSystem
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StudentSystemContext db = new StudentSystemContext();
+
+            db.Database.EnsureCreated();
+
+            Console.WriteLine("Database created successfully!");
+        }
+    }
+}
