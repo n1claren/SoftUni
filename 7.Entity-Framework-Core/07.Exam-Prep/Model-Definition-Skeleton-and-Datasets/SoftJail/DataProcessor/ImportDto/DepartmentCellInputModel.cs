4b0aa4da51fc62d865e80b70ad9e7d61b6151d8a
Nikolay Ivanov <nikolay1892@abv.bg>
2021-12-03 02:03:58 +0200

    exam prep

---
+++ 7.Entity-Framework-Core/07.Exam-Prep/Model-Definition-Skeleton-and-Datasets/SoftJail/DataProcessor/ImportDto/DepartmentCellInputModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    public class DepartmentCellInputModel
+    {
+        [Required]
+        [StringLength(25, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        public IEnumerable<CellInputModel> Cells { get; set; }
+    }
+}
---
+++ 7.Entity-Framework-Core/07.Exam-Prep/Model-Definition-Skeleton-and-Datasets/SoftJail/DataProcessor/Serializer.cs
@@ -0,0 +1,68 @@
+﻿namespace SoftJail.DataProcessor
+{
+
+    using Data;
+    using Newtonsoft.Json;
+    using SoftJail.DataProcessor.ExportDto;
+    using System;
+    using System.Linq;
+
+    public class Serializer
+    {
+        public static string ExportPrisonersByCells(SoftJailDbContext context, int[] ids)
+        {
+            var result = context
+                .Prisoners
+                .Where(x => ids.Contains(x.Id))
+                .Select(x => new
+                {
+                    Id = x.Id,
+                    Name = x.FullName,
+                    CellNumber = x.Cell.CellNumber,
+                    Officers = x.PrisonerOfficers.Select(o => new
+                    {
+                        OfficerName = o.Officer.FullName,
+                        Department = o.Officer.Department.Name
+                    })
+                    .ToList(),
+                    TotalOfficerSalary = decimal.Parse(x.PrisonerOfficers
+                                          .Sum(x => x.Officer.Salary)
+                                          .ToString("F2"))
+                })
+                .OrderBy(x => x.Name)
+                .ThenBy(x => x.Id)
+                .ToList();
+
+            string json = JsonConvert.SerializeObject(result, Formatting.Indented);
+
+            return json;
+        }
+
+        public static string ExportPrisonersInbox(SoftJailDbContext context, string prisonersNames)
+        {
+            var names = prisonersNames.Split(',', StringSplitOptions.RemoveEmptyEntries);
+
+            var result = context
+                .Prisoners
+                .Where(x => names.Contains(x.FullName))
+                .Select(x => new PrisonerViewModel
+                {
+                    Id = x.Id,
+                    Name = x.FullName,
+                    IncarcerationDate = x.IncarcerationDate.ToString("yyyy-MM-dd"),
+                    EncryptedMessages = x.Mails.Select(x => new EncryptedMessageViewModel
+                    {
+                        Description = string.Join("", x.Description.Reverse())
+                    })
+                    .ToArray()
+                })
+                .OrderBy(x => x.Name)
+                .ThenBy(x => x.Id)
+                .ToList();
+
+            var xml = XmlConverter.Serialize(result, "Prisoners");
+
+            return xml;
+        }
+    }
+}
