a49c18edf3e79300a2a695c5ad013cd1435afe3f
Nikolay Ivanov <nikolay1892@abv.bg>
2022-01-23 00:14:14 +0200

    added namespace, class and void method

--- 8.C#-Web-Basics/01.Web-Server-HTTP-Protocol/BasicWebServer/BasicWebServer.Demo/StartUp.cs
+++ 8.C#-Web-Basics/01.Web-Server-HTTP-Protocol/BasicWebServer/BasicWebServer.Demo/StartUp.cs
@@ -1,12 +1,34 @@
 ﻿using BasicWebServer.Server;
-using BasicWebServer.Server.Common;
+using BasicWebServer.Server.HTTP;
 using BasicWebServer.Server.Responses;
 
-void Main() => new HttpServer(routes => routes
+namespace BasicWebServer.Demo
+{
+    public class StartUp
+    {
+        public const string HtmlForm = @"<form action='/HTML' method='POST'>
+   Name: <input type='text' name='Name'/>
+   Age: <input type='number' name ='Age'/>
+<input type='submit' value ='Save' />
+</form>";
+
+        public static void Main()
+            => new HttpServer(routes => routes
                  .MapGet("/", new TextResponse("Hello from the server!"))
                  .MapGet("/Redirect", new RedirectResponse("https://softuni.org"))
-                 .MapGet("/HTML", new HtmlResponse(Constants.HtmlForm))
-                 .MapPost("/HTML", new TextResponse("", Constants.AddFormDataAction)))
+                 .MapGet("/HTML", new HtmlResponse(HtmlForm))
+                 .MapPost("/HTML", new TextResponse("", AddFormDataAction)))
               .Start();
 
-Main();
+        public static void AddFormDataAction(Request request, Response response)
+        {
+            response.Body = "";
+
+            foreach (var (key, value) in request.Form)
+            {
+                response.Body += $"{key} - {value}";
+                response.Body += Environment.NewLine;
+            }
+        }
+    }
+}
