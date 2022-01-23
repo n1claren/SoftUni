1744da6e12774623c82a2c11a66db51f085b1217
Nikolay Ivanov <nikolay1892@abv.bg>
2022-01-23 16:32:19 +0200

    made the web server run asynchronously

---
+++ 8.C#-Web-Basics/02.Web-Server-Asynchronous-Processing/BasicWebServer2.0/BasicWebServer.Server/Responses/ContentResponse.cs
@@ -0,0 +1,33 @@
+﻿using BasicWebServer.Server.Common;
+using BasicWebServer.Server.HTTP;
+using System.Text;
+
+namespace BasicWebServer.Server.Responses
+{
+    public class ContentResponse : Response
+    {
+        public ContentResponse(string content, string contentType, Action<Request, Response> preRenderAction = null) 
+            : base(StatusCode.OK)
+        {
+            Guard.AgainstNull(content, nameof(content));
+            Guard.AgainstNull(contentType, nameof(contentType));
+
+            this.PreRenderAction = preRenderAction;
+
+            this.Headers.Add(Header.ContentType, contentType);
+
+            this.Body = content;
+        }
+
+        public override string ToString()
+        {
+            if (this.Body != null)
+            {
+                var contentLength = Encoding.UTF8.GetByteCount(this.Body).ToString();
+                this.Headers.Add(Header.ContentLength, contentLength);
+            }
+
+            return base.ToString();
+        }
+    }
+}
