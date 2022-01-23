0512b51f983b64e21211b5bd2617befa10ffc62b
Nikolay Ivanov <nikolay1892@abv.bg>
2022-01-23 15:45:36 +0200

    chronometer

---
+++ 8.C#-Web-Basics/02.Web-Server-Asynchronous-Processing/Chronometer/Chronometer/Chronometerr.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+
+namespace Chronometer
+{
+    public class Chronometerr : IChronometer
+    {
+        private Stopwatch stopWatch;
+        private List<string> laps;
+
+        public Chronometerr()
+        {
+            this.stopWatch = new Stopwatch();
+            this.laps = new List<string>();
+        }
+
+        public string GetTime => this.stopWatch.Elapsed.ToString(@"mm\:ss\.ffff");
+
+        public List<string> Laps => this.laps;
+
+        public string Lap()
+        {
+            string result = GetTime;
+            this.laps.Add(result);
+            return result;
+        }
+
+        public void Reset()
+        {
+            this.stopWatch.Reset();
+            this.laps.Clear();
+        }
+
+        public void Start()
+        {
+            this.stopWatch.Start();
+        }
+
+        public void Stop()
+        {
+            this.stopWatch.Stop();
+        }
+    }
+}
---
+++ 8.C#-Web-Basics/02.Web-Server-Asynchronous-Processing/Chronometer/Chronometer/StartUp.cs
@@ -0,0 +1,49 @@
+﻿using Chronometer;
+
+var chronometer = new Chronometerr();
+
+string line;
+
+while ((line = Console.ReadLine().ToLower()) != "exit")
+{
+    if (line == "start")
+    {
+        Task.Run(() =>
+        {
+            chronometer.Start();
+        });
+    }
+    else if (line == "stop")
+    {
+        chronometer.Stop();
+    }
+    else if (line == "lap")
+    {
+        Console.WriteLine(chronometer.Lap());
+    }
+    else if (line == "laps")
+    {
+        if (chronometer.Laps.Count == 0)
+        {
+            Console.WriteLine("Laps: no laps");
+            continue;
+        }
+
+        Console.WriteLine("Laps: ");
+
+        for (int i = 0; i < chronometer.Laps.Count; i++)
+        {
+            Console.WriteLine($"{i+1} - ({chronometer.Laps[i]})");
+        }
+    }
+    else if (line == "reset")
+    {
+        chronometer.Reset();
+    }
+    else if (line == "time")
+    {
+        Console.WriteLine(chronometer.GetTime);
+    }
+}
+
+chronometer.Stop();
